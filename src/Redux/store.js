1cac120a41ca637978eb5527efa0e8a7f62c26fd
nikdmitrys <tipok24rus@gmail.com>
2020-06-08 19:37:43 +0700

    import form

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,8 @@
-import {createStore, combineReducers} from "redux"
-import {neighborsPage} from "./neighborsReducer.js"
-import {formPage} from "./formReducer.js"
-let reducers = combineReducers({neighborsPage, formPage});
-let store = createStore(reducers);
+import { createStore, combineReducers } from "redux"
+import { neighborsPage } from "./neighborsReducer.js"
+import { formPage } from "./formReducer.js"
+import { reducer as form } from "redux-form"
+let reducers = combineReducers( { neighborsPage, formPage, form } );
+let store = createStore( reducers );
 
 export default store;
